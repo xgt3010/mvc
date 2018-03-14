34928c09f06050c32b20492ad101b178c7735000
xgt3010 <37256273+xgt3010@users.noreply.github.com>
2018-03-14 14:16:26 +0800

    mvc

---
+++ mvc_8/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = [
+	// 数据库配置
+	'db' => [
+		'host' => 'localhost',
+		'username' => 'root',
+		'password' => 'root',
+		'dbname' => '',
+	],
+	// 默认控制器和操作名称
+	'defaultController' => '',
+	'defaultAction' => '',
+];
+return $config;
---
+++ mvc_8/fastphp/Fastphp.php
@@ -0,0 +1,178 @@
+<?php
+namespace fastphp;
+
+// 框架根目录
+defined('CORE_PATH') or define('CORE_PATH', __DIR__);
+
+/**
+ * fastphp框架核心
+ */
+class Fastphp
+{
+    // 配置内容
+    protected $config = [];
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    // 运行程序
+    public function run()
+    {
+    	// 作用类似于__autoload
+        spl_autoload_register(array($this, 'loadClass'));
+        // 检测开发环境
+        $this->setReporting();
+        // 检测敏感字符并删除
+        $this->removeMagicQuotes();
+        // 检测自定义全局变量并移除
+        $this->unregisterGlobals();
+        // 配置数据库信息
+        $this->setDbConfig();
+        // 路由处理
+        $this->route();
+    }
+
+    // 路由处理
+    public function route()
+    {
+    	// 获取默认控制器和操作名称
+        $controllerName = $this->config['defaultController'];
+        $actionName = $this->config['defaultAction'];
+        $param = array();
+        //  获取域名后的URL
+        $url = $_SERVER['REQUEST_URI'];
+        // 清除?之后的内容
+        $position = strpos($url, '?');
+        $url = $position === false ? $url : substr($url, 0, $position);
+        // 删除前后的“/”
+        $url = trim($url, '/');
+
+        if ($url) {
+            // 使用“/”分割字符串，并保存在数组中
+            $urlArray = explode('/', $url);
+            // 删除空的数组元素
+            $urlArray = array_filter($urlArray);
+            
+            // 获取控制器名  首字母大写
+            $controllerName = ucfirst($urlArray[0]);
+            
+            // 获取动作名
+            array_shift($urlArray);
+            $actionName = $urlArray ? $urlArray[0] : $actionName;
+            
+            // 获取URL参数
+            array_shift($urlArray);
+            $param = $urlArray ? $urlArray : array();
+        }
+
+        // 判断控制器和操作是否存在
+        $controller = 'app\\controllers\\'. $controllerName . 'Controller';
+        if (!class_exists($controller)) {
+            exit($controller . '控制器不存在');
+        }
+        if (!method_exists($controller, $actionName)) {
+            exit($actionName . '方法不存在');
+        }
+
+        // 如果控制器和操作名存在，则实例化控制器，因为控制器对象里面
+        // 还会用到控制器名和操作名，所以实例化的时候把他们俩的名称也
+        // 传进去。结合Controller基类一起看
+        $dispatch = new $controller($controllerName, $actionName);
+
+        // $dispatch保存控制器实例化后的对象，我们就可以调用它的方法
+        call_user_func_array(array($dispatch, $actionName), $param);
+    }
+
+    // 检测开发环境
+    public function setReporting()
+    {
+    	//判断是调试阶段还是上线阶段  true  false
+        if (APP_DEBUG === true) {
+            error_reporting(E_ALL);  // 报告所有错误
+            ini_set('display_errors','On');  // 开启错误回显，若出现错误，则报错，出现错误提示
+        } else {
+            error_reporting(E_ALL);
+            ini_set('display_errors','Off'); //  关闭错误回显，若出现错误，则提示：服务器错误。但是不会出现错误提示
+            ini_set('log_errors', 'On'); //  开启日志
+        }
+    }
+
+    // 删除敏感字符
+    public function stripSlashesDeep($value)
+    {
+        $value = is_array($value) ? array_map(array($this, 'stripSlashesDeep'), $value) : stripslashes($value);
+        return $value;
+    }
+
+    // 检测敏感字符并删除
+    public function removeMagicQuotes()
+    {
+        if (get_magic_quotes_gpc()) {
+            $_GET = isset($_GET) ? $this->stripSlashesDeep($_GET ) : '';
+            $_POST = isset($_POST) ? $this->stripSlashesDeep($_POST ) : '';
+            $_COOKIE = isset($_COOKIE) ? $this->stripSlashesDeep($_COOKIE) : '';
+            $_SESSION = isset($_SESSION) ? $this->stripSlashesDeep($_SESSION) : '';
+        }
+    }
+
+    // 检测自定义全局变量并移除
+    public function unregisterGlobals()
+    {
+        if (ini_get('register_globals')) {
+            $array = array('_SESSION', '_POST', '_GET', '_COOKIE', '_REQUEST', '_SERVER', '_ENV', '_FILES');
+            foreach ($array as $value) {
+                foreach ($GLOBALS[$value] as $key => $var) {
+                    if ($var === $GLOBALS[$key]) {
+                        unset($GLOBALS[$key]);
+                    }
+                }
+            }
+        }
+    }
+
+    // 配置数据库信息
+    public function setDbConfig()
+    {
+        if ($this->config['db']) {
+            define('DB_HOST', $this->config['db']['host']);
+            define('DB_NAME', $this->config['db']['dbname']);
+            define('DB_USER', $this->config['db']['username']);
+            define('DB_PASS', $this->config['db']['password']);
+        }
+    }
+
+    // 自动加载类
+    public function loadClass($className)
+    {
+        $classMap = $this->classMap();
+
+        if (isset($classMap[$className])) {
+            // 包含内核文件
+            $file = $classMap[$className];
+        } elseif (strpos($className, '\\') !== false) {
+            // 包含应用（application目录）文件
+            $file = APP_PATH . str_replace('\\', '/', $className) . '.php';
+            if (!is_file($file)) {
+                return;
+            }
+        } else {
+            return;
+        }
+
+        include $file;
+    }
+
+    // 内核文件命名空间映射关系
+    protected function classMap()
+    {
+        return [
+            'fastphp\base\Controller' => CORE_PATH . '/base/Controller.php',
+            'fastphp\base\Model' => CORE_PATH . '/base/Model.php',
+            'fastphp\base\View' => CORE_PATH . '/base/View.php',
+            'fastphp\db\Db' => CORE_PATH . '/db/Db.php',
+            'fastphp\db\Sql' => CORE_PATH . '/db/Sql.php',
+        ];
+    }
+}
